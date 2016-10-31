8f010d21d16f56025b1acbc3f0de04f7b37a0ae9
viohlet <violetavenegas@hotmail.com>
2016-10-31 16:39:34 -0400

    Text and basic structure changes
    Text and basic structure changes. Added images and text for
    about me, school sections.
    Next steps: bring up the experience section and complete it.

--- js/main.js
+++ js/main.js
@@ -143,7 +143,7 @@
 
   // Typed Js
   $(".element").typed({
-    strings: ["I'm Web Developer", "I'm Web Designer"],
+    strings: ["Full Stack Web Developer"],
     typeSpeed: 1,
     backSpeed: 1,
     backDelay: 1000,
@@ -151,7 +151,7 @@
   });
 
   $(".sub-title").typed({
-    strings: ["Web Developer", "Web Designer"],
+    strings: ["Web Developer", "Latina", "happy to meet you!"],
     typeSpeed: 1,
     backSpeed: 1,
     backDelay: 1400,
